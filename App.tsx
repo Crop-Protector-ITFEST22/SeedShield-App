385f8192e073bd622f961e36952c8b15745bfe41
Aktindo <akshit.singla.dps@gmail.com>
2022-08-20 06:42:04 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+  View,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    Alert.alert("You clicked the button!", "Haha yes", [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Press me" onPress={handlePress} />
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#C8EEBA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    marginTop: 20,
   },
 });
